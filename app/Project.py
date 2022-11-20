a46ce3f76fb3fa1618973065be11daa9caae71e1
Mathieu Du Jardin <72858870+MathieuDJ@users.noreply.github.com>
2022-11-20 14:32:36 +0100

    Update Project.py

--- app/Project.py
+++ app/Project.py
@@ -6,7 +6,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "*"
+     "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:27376",
+    "http://127.0.0.1:27376",
+    "https://randomizer-service-mathieudj.cloud.okteto.net",
+    "https://MathieuDJ.github.io",
+    "https://mathieudj.github.io."
 ]
 
 app.add_middleware(
